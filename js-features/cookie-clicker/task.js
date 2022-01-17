7d93cfe5a99396d7727423ddc66e4ada100a1d7a
Asanbekov-N <80524722+Asanbekov-N@users.noreply.github.com>
2022-01-17 10:55:10 +0700

    Add files via upload
    ch

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,9 +1,17 @@
 const clickerCounter = document.getElementById("clicker__counter");
 const cookie = document.getElementById("cookie");
+const clickSpeed = document.getElementById("click__speed");
+let date = new Date ()
 
 cookie.onclick = function() {
   clickerCounter.textContent ++;
-  cookie.width === 200
+    cookie.width === 200
   ? (cookie.width = 180)
   : (cookie.width = 200);
+
+  let newDate = new Date();
+  let speed = (1 / ((newDate - date) / 1000)).toFixed(2);
+  date = new Date ();
+  clickSpeed.textContent = speed;
 }
+
